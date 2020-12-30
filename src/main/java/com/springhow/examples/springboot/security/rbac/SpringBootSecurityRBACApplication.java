f81f8124d0b6a9d4f21dce49f7fa2772287ba8f3
Raja Anbazhagan <raja.anbazhagan1991@gmail.com>
2020-12-30 18:34:43 +0530

    removed unwanted code

--- src/main/java/com/springhow/examples/springboot/security/rbac/SpringBootSecurityRBACApplication.java
+++ src/main/java/com/springhow/examples/springboot/security/rbac/SpringBootSecurityRBACApplication.java
@@ -11,11 +11,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SuppressWarnings("ALL")
-@SpringBootApplication
-public class SpringBootSecurityRBACApplication implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(SpringBootSecurityRBACApplication.class);
+@SpringBootApplication
+public class SpringBootSecurityRBACApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityRBACApplication.class, args);
@@ -26,15 +24,4 @@ public class SpringBootSecurityRBACApplication implements CommandLineRunner {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    @Override
-    public void run(String... args) {
-        logger.info("{}", jdbcTemplate.query("select id,role_name from user_role", (rs, rowNum) -> rs.getString(1) + rs.getString(2)));
-        logger.info("{}", jdbcTemplate.query("select id,privilege_name from user_privilege", (rs, rowNum) -> rs.getString(1) + rs.getString(2)));
-        logger.info("{}", jdbcTemplate.query("select id,username,password,active from user_account", (rs, rowNum) -> rs.getString(1) + rs.getString(2) + rs.getString(3) + rs.getString(4)));
-        logger.info("{}", jdbcTemplate.query("select id,role_id,privilege_id from user_role_to_privilege", (rs, rowNum) -> rs.getString(1) + rs.getString(2) + rs.getString(3)));
-        logger.info("{}", jdbcTemplate.query("select id,user_id,role_id from user_to_role", (rs, rowNum) -> rs.getString(1) + rs.getString(2) + rs.getString(3)));
-    }
 }
